a82171d63071c4eacf1a22ed63b95f8bdea2d0c2
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-04-04 22:20:50 -0300

    refactor: Refactor icon prop in Card component
    * Updated the implementation of the icon prop in the Card component for improved maintainability.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,18 +1,27 @@
+import type { IconProps } from "phosphor-react-native";
+import { useTheme } from "styled-components/native";
 import { Icon, Subtitle, Title, Wrapper } from "./styles";
 
 interface CardProps {
   title: string;
   subtitle: string;
-  icon?: React.JSX.Element;
+  icon?: (props: IconProps) => JSX.Element;
 }
 
 export function Card({ title, subtitle, icon }: CardProps) {
+  const { color } = useTheme();
+  const IconElement = icon;
+
   return (
     <Wrapper>
       <Title>{title}</Title>
       <Subtitle>{subtitle}</Subtitle>
 
-      {icon && <Icon>{icon}</Icon>}
+      {IconElement && (
+        <Icon>
+          <IconElement size={24} color={color.green700} />
+        </Icon>
+      )}
     </Wrapper>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -20,7 +20,7 @@ export function Home() {
         <Card
           title="90,86%"
           subtitle="das refeições dentro da dieta"
-          icon={<ArrowUpRight size={24} color={color.green700} />}
+          icon={ArrowUpRight}
         />
       </TouchableOpacity>
 
