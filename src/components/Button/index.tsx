9f2aa05b98802612ddf07f04f2c5e096183d8124
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-04-19 23:33:10 -0300

    feat: Add primary and secondary types to Button component
    * Modified the Button component to support both primary and secondary button types, providing users with more options for styling and functionality.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,28 @@
 import type { TouchableOpacityProps } from "react-native";
 import type { IconProps } from "phosphor-react-native";
 import { useTheme } from "styled-components/native";
-import { Title, Wrapper } from "./styles";
+import { ButtonType, Title, Wrapper } from "./styles";
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
+  type?: ButtonType;
   icon?: (props: IconProps) => JSX.Element;
 }
 
-export function Button({ title, icon, ...rest }: ButtonProps) {
+export function Button({
+  title,
+  type = "primary",
+  icon,
+  ...rest
+}: ButtonProps) {
   const { color } = useTheme();
   const Icon = icon;
+  const iconColor = type === "primary" ? color.white : color.gray700;
 
   return (
-    <Wrapper {...rest}>
-      {Icon && <Icon size={18} color={color.white} />}
-      <Title>{title}</Title>
+    <Wrapper type={type} {...rest}>
+      {Icon && <Icon size={18} color={iconColor} />}
+      <Title type={type}>{title}</Title>
     </Wrapper>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,20 +1,32 @@
 import styled, { css } from "styled-components/native";
 
-export const Wrapper = styled.TouchableOpacity`
+export type ButtonType = "primary" | "secondary";
+
+export const Wrapper = styled.TouchableOpacity<{ type: ButtonType }>`
   flex-direction: row;
   justify-content: center;
   align-items: center;
   column-gap: 12px;
   min-height: 50px;
   max-height: 50px;
-  background-color: ${({ theme }) => theme.color.gray600};
-  border-radius: 6px;
   padding: 16px 24px;
+  border-radius: 6px;
+  border-width: 1px;
+  border-color: ${({ theme }) => theme.color.gray600};
+
+  ${({ theme, type }) =>
+    type === "primary"
+      ? css`
+          background-color: ${theme.color.gray600};
+        `
+      : css`
+          background-color: transparent;
+        `};
 `;
 
-export const Title = styled.Text`
-  ${({ theme }) => css`
-    color: ${theme.color.white};
+export const Title = styled.Text<{ type: ButtonType }>`
+  ${({ theme, type }) => css`
+    color: ${type === "primary" ? theme.color.white : theme.color.gray700};
     font-family: ${theme.fontFamily.bold};
     font-size: ${theme.fontSize.sm}px;
   `}
